72c7947cbe533fe42dd9b9948fe7bc391dd7e02d
Daniel <thed4nm4n@gmail.com>
2021-11-16 20:10:46 -0500

    Moved to global command registration

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,7 +1,6 @@
 const fs = require("fs")
 const { REST } = require("@discordjs/rest")
 const { Routes } = require("discord-api-types/v9")
-const { clientId, developmentGuildId, token } = require("./config.json")
 
 const commands = []
 const commandFiles = fs
@@ -13,10 +12,10 @@ for (const file of commandFiles) {
   commands.push(command.data.toJSON())
 }
 
-const rest = new REST({ version: "9" }).setToken(token)
+const rest = new REST({ version: "9" }).setToken(process.env.TOKEN)
 
 rest
-  .put(Routes.applicationGuildCommands(clientId, developmentGuildId), {
+  .put(Routes.applicationCommands(process.env.CLIENT_ID), {
     body: commands,
   })
   .then(() => console.log("Successfully registered application commands."))
